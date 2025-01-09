88c307f1381d4f08c5e0b2a0283f899253101cc2
Pablo-Carreras <pablo.carreras-canton@mths.ca>
2025-01-09 11:25:34 -0500

    did code

--- main.ts
+++ main.ts
@@ -1,8 +1,54 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Pablo and Mark 
+ * Created on: Jan 2025
+ * This program makes the motors move 
 */
 
-basic.showString('Hello, World!')
+ //Variables 
+let neopixelStrip: neopixel.Strip = null
+let distanceOfObject: number
+
+    // setup
+    basic.showIcon(IconNames.Happy)
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+
+    // loop
+    while (true) {
+        distanceOfObject = sonar.ping(
+            DigitalPin.P1,
+            DigitalPin.P2,
+            PingUnit.Centimeters
+        )
+
+        // forward
+        if (distanceOfObject > 10) {
+            basic.showIcon(IconNames.Yes)
+            robotbit.StpCarMove(10, 48)
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+            neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+            neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+            neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+            neopixelStrip.show()
+
+        } else {
+
+            // stop
+            basic.showIcon(IconNames.No)
+            robotbit.StpCarMove(0, 0)
+            basic.pause(500) 
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+            neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+            neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+            neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+            neopixelStrip.show()
+
+}
+    } 
