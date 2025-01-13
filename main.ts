f8852f87b4541055550c7b4402beab7309d151a1
Pablo-Carreras <pablo.carreras-canton@mths.ca>
2025-01-13 11:22:29 -0500

    did the code

--- main.ts
+++ main.ts
@@ -5,50 +5,29 @@
  * This program makes the motors move 
 */
 
- //Variables 
-let neopixelStrip: neopixel.Strip = null
+// setup
+basic.showIcon(IconNames.Happy)
 let distanceOfObject: number
 
-    // setup
-    basic.showIcon(IconNames.Happy)
-neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.show()
-basic.showIcon(IconNames.Happy)
+// loop
+input.onButtonPressed(Button.A, function () {
+while (true) {
+    distanceOfObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
 
+    // forward
+    if (distanceOfObject > 10) {
+        basic.showIcon(IconNames.Yes)
+        robotbit.StpCarMove(10, 48)
+    } else {
 
-    // loop
-    while (true) {
-        distanceOfObject = sonar.ping(
-            DigitalPin.P1,
-            DigitalPin.P2,
-            PingUnit.Centimeters
-        )
-
-        // forward
-        if (distanceOfObject > 10) {
-            basic.showIcon(IconNames.Yes)
-            robotbit.StpCarMove(10, 48)
-            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
-            neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
-            neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
-            neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
-            neopixelStrip.show()
-
-        } else {
-
-            // stop
-            basic.showIcon(IconNames.No)
-            robotbit.StpCarMove(0, 0)
-            basic.pause(500) 
-            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
-            neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
-            neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
-            neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
-            neopixelStrip.show()
-
+        // stop
+        basic.showIcon(IconNames.No)
+        robotbit.StpCarMove(0, 0)
+        basic.pause(500)
+    }
 }
-    } 
+})
