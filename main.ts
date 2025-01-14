75e35f3aec1b1853362fd4d7351f198781c0ed46
Pablo-Carreras <pablo.carreras-canton@mths.ca>
2025-01-14 10:46:43 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,23 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+ /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: Pablo and Mark 
  * Created on: Jan 2025
  * This program makes the motors move 
 */
 
+let distanceOfObject: number
+let neopixelStrip: neopixel.Strip = null
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.clearScreen()
+
+
 // setup
 basic.showIcon(IconNames.Happy)
-let distanceOfObject: number
 
 // loop
 input.onButtonPressed(Button.A, function () {
@@ -19,15 +29,27 @@ while (true) {
     )
 
     // forward
-    if (distanceOfObject > 10) {
+    if (distanceOfObject > 15) {
         basic.showIcon(IconNames.Yes)
         robotbit.StpCarMove(10, 48)
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.show()
+
     } else {
 
         // stop
         basic.showIcon(IconNames.No)
         robotbit.StpCarMove(0, 0)
         basic.pause(500)
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.show()
+
     }
 }
 })
